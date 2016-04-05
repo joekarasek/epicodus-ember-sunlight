a2feb4e599133068a15e32eb1ff64af46b66be16
Joseph Karasek and Josh Overly <joshoverly@yahoo.com>
2016-04-05 10:30:36 -0700

    add display of presidential candidate info

--- app/routes/finance.js
+++ app/routes/finance.js
@@ -4,8 +4,8 @@ export default Ember.Route.extend({
   model: function(params) {
     var url = 'http://realtime.influenceexplorer.com/api/candidates/?fec_id=' + params.fec_id + '&apikey=91ac7ae7c39c47fbaf3508b258fc2d7d';
     return Ember.$.getJSON(url).then(function(responseJSON) {
-      console.log(responseJSON.results);
-      return responseJSON.results;
+      console.log(responseJSON.results[0]);
+      return responseJSON.results[0];
     });
    }
 });

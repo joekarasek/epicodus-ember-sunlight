577963fff393d331c247799c7ea221d745ab982d
Joseph Karasek and Josh Overly <joshoverly@yahoo.com>
2016-04-05 10:20:10 -0700

    add dynmaic routes to grab data for presidential candidates

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('results', { path: '/results/:zip'});
-  this.route('finance');
+  this.route('finance', { path: '/finance/:fec_id'});
 });
 
 export default Router;

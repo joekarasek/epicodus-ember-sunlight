bf64384176434ebcd475f0f9b37183ef18f68612
Joseph Karasek and Josh Overly <joseph.karasek@gmail.com>
2016-04-05 08:47:51 -0700

    add display of legislatures to app

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
+
 export default Ember.Route.extend({
+ model: function(params) {
+   var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=91ac7ae7c39c47fbaf3508b258fc2d7d&zip=' + params.zip;
+   return Ember.$.getJSON(url).then(function(responseJSON) {
+     return responseJSON.results;
+   });
+  }
 });
